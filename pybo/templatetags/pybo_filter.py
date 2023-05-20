5604e2400b187fda76af1c594336e90eb2ebb67b
GaBaljaintheroom <junsu1222@naver.com>
2023-05-20 23:40:15 +0900

    feat: 마크다운

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -10,4 +10,7 @@ def sub(value, arg):
     return value - arg
 
 
-
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
