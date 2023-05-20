0af42d3105ba3396ba644628872fa0da0bb7351e
GaBaljaintheroom <junsu1222@naver.com>
2023-05-20 17:07:22 +0900

    feat : 페이징

--- pybo/views.py
+++ pybo/views.py
@@ -2,14 +2,18 @@
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 from .forms import QuestionForm, AnswerForm
 from .models import Question
 
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
