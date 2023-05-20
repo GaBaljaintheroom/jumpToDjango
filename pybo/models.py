a194366c81591c389e1ba406fbb8089ed2e33e82
GaBaljaintheroom <junsu1222@naver.com>
2023-05-20 12:56:06 +0900

    feat: 장고 관리자

--- pybo/models.py
+++ pybo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Question(models.Model):
     subject = models.CharField(max_length=200)
@@ -8,6 +9,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.subject
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
